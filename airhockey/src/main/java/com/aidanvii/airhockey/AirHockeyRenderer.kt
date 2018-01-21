94370bba524f83b261f99be7622c7a1f4f6f4a3e
Aidan McWilliams <aidan.vii@googlemail.com>
2018-01-21 22:44:43 +0100

    Chapter 2 - Defining Vertices & Shaders

--- airhockey/src/main/java/com/aidanvii/airhockey/AirHockeyRenderer.kt
+++ airhockey/src/main/java/com/aidanvii/airhockey/AirHockeyRenderer.kt
@@ -1,9 +1,39 @@
 package com.aidanvii.airhockey
 
 import com.aidanvii.utils.opengles.v20.OpenGLES2Renderer
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 import javax.microedition.khronos.egl.EGLConfig
 
 class AirHockeyRenderer : OpenGLES2Renderer() {
+
+    val tableVerticesWithTriangles = floatArrayOf(
+            // triangle 1
+            0f, 0f,
+            9f, 14f,
+            0f, 14f,
+            //triangle 2
+            0f, 0f,
+            9f, 0f,
+            9f, 14f,
+            // line
+            0f, 7f,
+            9f, 7f,
+            // mallets
+            4.5f, 2f,
+            4.5f, 12f
+    )
+
+    val vertexData = ByteBuffer
+            // allocate block of native memory
+            .allocateDirect(tableVerticesWithTriangles.size * BYTES_PER_FLOAT)
+            // order the bytes in the platforms native order (http://en.wikipedia.org/wiki/Endianness)
+            .order(ByteOrder.nativeOrder())
+            // convert to FloatBuffer to not deal with bytes directly
+            .asFloatBuffer().apply {
+        // copy memory from Android runtime to native memory
+        put(tableVerticesWithTriangles)
+    }
 
     override fun onSurfaceCreated(config: EGLConfig) {
         glClearColor(red = 1f, blue = 1f)
@@ -15,5 +45,9 @@ class AirHockeyRenderer : OpenGLES2Renderer() {
 
     override fun onDrawFrame() {
         glClear()
+    }
+
+    companion object {
+        const val BYTES_PER_FLOAT = 4
     }
 }
---
+++ utils/src/main/res/raw/simple_vertex_shader.glsl
@@ -0,0 +1,7 @@
+# TODO document from book!
+
+attribute vec4 a_position;
+
+void main() {
+    gl_Position = a_Position;
+}
